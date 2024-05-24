c1e1d90cf445f12664291c9a52b13d342cb5ee56
aismailanji <107570938+aismailanji@users.noreply.github.com>
2024-05-24 18:05:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // document.getElementById('fetch-lore').addEventListener('click', async () => {
-//     console.log('Button clicked'); // Debugging statement
+//     console.log('Button clicked'); // debugging
 //     try {
 //         const response = await fetch('/lore');
-//         console.log('Request sent'); // Debugging statement
+//         console.log('Request sent'); // debugging
 //         const data = await response.json();
-//         console.log('Data received:', data); // Debugging statement
+//         console.log('Data received:', data); // debugging
 //         document.getElementById('lore-text').innerText = data.lore;
 //     } catch (error) {
 //         console.error('Error fetching lore:', error);
